88f88c12528eb81ad37e004cd9df925b518adcbb
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-07-01 02:00:10 +0000

    📸 taking-photos

--- photo-gallery/src/composables/usePhotoGallery.ts
+++ photo-gallery/src/composables/usePhotoGallery.ts
@@ -3,6 +3,13 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
+const photos = ref<UserPhoto[]>([]);
+
 export const usePhotoGallery = () => {
     const takePhoto = async () => {
       const photo = await Camera.getPhoto({
@@ -10,9 +17,18 @@ export const usePhotoGallery = () => {
         source: CameraSource.Camera,
         quality: 100,
       });
+      
+      const fileName = Date.now() + '.jpeg';
+      const savedFileImage = {
+        filepath: fileName,
+        webviewPath: photo.webPath
+      };
+
+      photos.value = [savedFileImage, ...photos.value];
     };
   
     return {
+      photos,
       takePhoto,
     };
   };
--- photo-gallery/src/views/Tab2Page.vue
+++ photo-gallery/src/views/Tab2Page.vue
@@ -6,18 +6,28 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="takePhoto()">
           <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button>
       </ion-fab>
+    
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { usePhotoGallery } from '@/composables/usePhotoGallery';
-const { takePhoto } = usePhotoGallery();
+const { photos, takePhoto } = usePhotoGallery();
 import { camera, trash, close } from 'ionicons/icons';
 import { 
   IonPage, 
